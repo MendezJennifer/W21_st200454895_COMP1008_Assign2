c159745399af09a828c31c5b5addbfcabcb9ae1a
MendezJennifer <77803373+MendezJennifer@users.noreply.github.com>
2021-01-21 12:56:03 -0700

    Completed Challenge 4

--- src/WarmUpExcercises.java
+++ src/WarmUpExcercises.java
@@ -23,8 +23,21 @@ public class WarmUpExcercises
         for (int j = 0; j<101; j+=10)
         {
             //Shorter way of doing the if/else loop
-            System.out.println(j%20==0?j+" JAVA":j);
+            System.out.println(j%20==0? j+" JAVA" : j);
         }
 
+        //Create a method (see below) that evaluates if it is appropriate to Ski (snowDepth must be >30)
+        System.out.println("\nChallenge 4");
+        System.out.println("timeToSki(31) = " +timeToSki(31));
+        System.out.println("timeToSki(30) = " +timeToSki(30));
+
+
+    }
+
+    //Remember that new methods should be created outside the main method
+    //Method that evaluates if it is appropriate to Ski (if snowDepth is >30)
+    public static boolean timeToSki(int snowDepth)
+    {
+        return snowDepth>30;
     }
 }
