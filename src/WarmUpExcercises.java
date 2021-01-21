42959741ca912670fdc26f8e8e60d1f664c09d09
MendezJennifer <77803373+MendezJennifer@users.noreply.github.com>
2021-01-21 12:43:20 -0700

    Completed Challenges 2 & 3

--- src/WarmUpExcercises.java
+++ src/WarmUpExcercises.java
@@ -7,23 +7,24 @@ public class WarmUpExcercises
     public static void main(String[] args)
     {
         //Loop goes from 100 to 0 in steps of -5
+        //If number is divisible by 20, display JAVA
+        System.out.println("Challenge 1 & 3");
         for (int i = 100; i>-1; i-=5)
         {
             if(i%20==0)
-            {
                 System.out.println(i+" Java");
-            }
             else
                 System.out.println(i);
         }
 
-        /*
         //Loop goes from 0 to 100 in steps of 10
-        for (int i = 0; i<101; i+=10)
+        //If number is divisible by 20, display JAVA
+        System.out.println("\nChallenge 2 & 3");
+        for (int j = 0; j<101; j+=10)
         {
-            System.out.println(i);
+            //Shorter way of doing the if/else loop
+            System.out.println(j%20==0?j+" JAVA":j);
         }
 
-         */
     }
 }
