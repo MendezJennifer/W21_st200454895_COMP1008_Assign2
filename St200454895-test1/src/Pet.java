94a3ef16669b79f663339842c1b2d9462ec31475
MendezJennifer <77803373+MendezJennifer@users.noreply.github.com>
2021-02-11 10:33:29 -0700

    setName(), setType, setAge, and setColor complete

--- St200454895-test1/src/Pet.java
+++ St200454895-test1/src/Pet.java
@@ -4,6 +4,8 @@
  *Date: 11/Fec/2021
  */
 
+import java.util.Arrays;
+import java.util.List;
 
 public class Pet
 {
@@ -33,6 +35,39 @@ public class Pet
 
     public String getColor() {
         return color;
+    }
+
+    public void setName(String name) {
+        name=name.trim();
+        if(name.length()>=2)
+            this.name = name;
+        else
+            throw new IllegalArgumentException("Name must be at least 2 characters.");
+    }
+
+    public void setType(String type) {
+        type=type.toLowerCase();
+         List<String> validPetTypes= Arrays.asList("dog", "cat", "bird", "reptile", "fish");
+         if(validPetTypes.contains(type))
+            this.type = type;
+         else
+             throw new IllegalArgumentException(type+ " pet type is invalid, please select one of the following: "+validPetTypes);
+    }
+
+    public void setAge(int age) {
+        if(age>=0 && age<=60)
+            this.age = age;
+        else
+            throw new IllegalArgumentException("Pet age should be between 0 and 60 years.");
+    }
+
+    public void setColor(String color) {
+        color=color.toLowerCase();
+        List<String> validPetColors= Arrays.asList("yellow", "brown", "black", "blue", "red", "white", "grey");
+        if(validPetColors.contains(color))
+            this.color = color;
+        else
+            throw new IllegalArgumentException(color+" color is invalid, please select one of the following: "+validPetColors);
     }
 }
 
