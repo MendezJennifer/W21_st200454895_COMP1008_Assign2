334c25e40eb2d4436be9c9316e0b15e627817457
MendezJennifer <77803373+MendezJennifer@users.noreply.github.com>
2021-03-18 11:50:28 -0600

    Added validation to set methos in Person Class

--- W21Java2EdMuse/src/models/Person.java
+++ W21Java2EdMuse/src/models/Person.java
@@ -2,6 +2,7 @@ package models;
 
 import java.time.LocalDate;
 import java.time.Period;
+import java.util.Locale;
 
 public class Person
 {
@@ -16,28 +17,62 @@ public class Person
         setBirthday(birthday);
     }
 
+    /**
+     * This returns person's age in years based on their birthday and the current date
+     * @return
+     */
     public int getAge()
     {
         return Period.between(birthday,LocalDate.now()).getYears();
     }
 
+    /**
+     * First name needs to be comprised of alphabetic characters, starting with an upper case letter
+     * @param firstName
+     */
     public void setFirstName(String firstName)
     {
-        this.firstName = firstName;
+        firstName=firstName.substring(0,1).toUpperCase()+firstName.substring(1);
+        //setting a pattern, first character must be uppercase A to Z, the rest must be lowercase a to z
+        if(firstName.matches("[A-Z][a-z]*"))
+        {
+            this.firstName = firstName;
+        }
+        else
+            throw new IllegalArgumentException("First Name must start with an upper case letter and only contain alphabetic characters");
     }
 
+    /**
+     * Last name needs to be comprised of alphabetic characters, starting with an upper case letter
+     * @param lastName
+     */
     public void setLastName(String lastName)
     {
-        this.lastName = lastName;
+        lastName=lastName.substring(0,1).toUpperCase()+lastName.substring(1);
+        //setting a pattern, first character must be uppercase A to Z, the rest must be lowercase a to z
+        if(lastName.matches("[A-Z][a-z]*"))
+        {
+            this.lastName = lastName;
+        }
+        else
+            throw new IllegalArgumentException("Last Name must start with an upper case letter and only contain alphabetic characters");
     }
 
     public void setAddress(String address)
     {
-        this.address = address;
+        address=address.trim();
+        if(address.length()>=5 && address.length()<=30)
+        {
+            this.address = address;
+        }
+        else
+            throw new IllegalArgumentException("Address must be 5 to 30 characters");
     }
 
     public void setBirthday(LocalDate birthday)
     {
+        if (birthday.isAfter(LocalDate.now()))
+            throw new IllegalArgumentException("birthday cannot be in the future");
         this.birthday = birthday;
     }
 
